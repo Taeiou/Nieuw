ab67f34a7beb593a7ece2943e836641b7bc98269
Taeiou <tom_vaessen@hotmail.com>
2015-03-04 01:30:43 +0100

    Nu de atom.py  additie

--- atom.py
+++ atom.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
+#hier ook iets bij 0126
 
-###Is dit nieuwigheid?
+##Is dit nieuwigheid?
 
 import pygame,sys
 import win32api,win32console,win32gui,codecs
